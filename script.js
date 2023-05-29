10e498abe9eb814b2add72dcdf72517b610e87c1
BOGGALA YASWANTH REDDY <93768814+yash2700@users.noreply.github.com>
2023-05-29 13:35:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-//your JS code here. If required.
+// Function to execute when the DOM is loaded
+function onDOMLoad() {
+  var paragraph = document.createElement("p");
+  
+  paragraph.textContent = "DOM load success";
+  
+  document.body.appendChild(paragraph);
+}
+
+document.addEventListener("DOMContentLoaded", onDOMLoad);
